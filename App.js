9a516900f2af78e4a7dd55535355ac90a0fe86d9
Shreyasc13 <44752893+Shreyasc13@users.noreply.github.com>
2021-06-07 22:42:32 +0530

    sign up

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import {LandingPage} from './src/screens/landing';
+import {SignUpPage} from './src/screens/SignUp';
+
 
 
 
@@ -11,6 +13,8 @@ const AuthStack = createStackNavigator();
 export default () => (
   <NavigationContainer>
     <AuthStack.Navigator>
+    <AuthStack.Screen name="SignupPage" component={SignUpPage} options={{ headerShown:false }}/>
+
       <AuthStack.Screen name="LandingPage" component={LandingPage} options={{ headerShown:false }}/>
       {/* <AuthStack.Screen name="SignIn" component={SignIn}/> */}
       {/*<AuthStack.Screen name="CreateAccount" component={CreateAccount}/> */}
