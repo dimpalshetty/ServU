5a4cb1f50ca58c4c7485114b4aa734985c897d09
dimpalshetty <dimpalshetty2000@gmail.com>
2021-06-08 16:37:58 +0530

    Categories

---
+++ src/components/gridbuttons.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {
+  Text, Image,
+  StyleSheet,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from "react-native-vector-icons/Ionicons";
+
+const width = Dimensions.get('window').width
+const height = Dimensions.get('window').height
+
+
+
+const Gridbtn= ({text,icon}) => {
+return(
+  <TouchableOpacity 
+  style={styles.button} >
+  <Icon
+                                name={icon}
+                                size="small"
+                                style={{
+                                    fontSize: 40,
+                            color: '#583EF2',
+                        }}
+                            />
+          <Text style={styles.taskdes}> {text} </Text>
+        </TouchableOpacity>
+
+        );
+}
+
+
+
+const styles = StyleSheet.create({
+
+  
+    button: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: 18,
+      textAlign: 'center',
+      color: 'red',
+      fontWeight: "500",
+      top: 4,
+      borderTopRightRadius: 20,
+      borderBottomLeftRadius: 20,
+      borderBottomRightRadius: 20,
+      borderColor: '#583EF2',
+      borderWidth: 1.5,
+      width: width/3.7,
+      height: height/6
+    },
+
+    taskdes: {
+        marginTop: 2,
+        color: '#583EF2',
+        fontFamily: 'Futura',
+        fontSize: 16,
+
+    },
+
+  
+
+  
+});
+
+export default Gridbtn;
