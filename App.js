461f25afc12cd4b52eae3a770da79a78b67ce13f
Shreyasc13 <44752893+Shreyasc13@users.noreply.github.com>
2021-07-30 11:24:01 +0530

    sync 2

--- App.js
+++ App.js
@@ -24,27 +24,12 @@ const AuthStack = createStackNavigator();
 export default () => (
   <NavigationContainer>
     <AuthStack.Navigator>
-<<<<<<< Updated upstream
-      <AuthStack.Screen name="SelectWorker" component={SelectWorker} options={{ headerShown: false }} />
-      <AuthStack.Screen name="HomePage" component={HomeStack} options={{ headerShown: false }} />
-      <AuthStack.Screen name="WorkerProfile" component={WorkerProfile} options={{ headerShown: false }} />
-      <AuthStack.Screen name="Booking" component={Booking} options={{ headerShown: false }} />
-      <AuthStack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
-      <AuthStack.Screen name="LandingPage" component={LandingPage} options={{ headerShown: false }} />
-      <AuthStack.Screen name="SignUpPage" component={SignUpPage} options={{ headerShown: false }} />
-      <AuthStack.Screen name="BookingDetails" component={BookingDetails} options={{ headerShown: false }} />
-      <AuthStack.Screen name="Notifications" component={Notifications} options={{ headerShown: false }} />
+    <AuthStack.Screen name="SelectWorker" component={SelectWorker} options={{ headerShown: false }} />
 
-
-
-
-
-=======
     <AuthStack.Screen name="LandingPage" component={LandingPage} options={{ headerShown: false }} />
     <AuthStack.Screen name="SignUpPage" component={SignUpPage} options={{ headerShown: false }} />
     <AuthStack.Screen name="HomePage" component={HomeStack} options={{ headerShown: false }} />
 
-    <AuthStack.Screen name="SelectWorker" component={SelectWorker} options={{ headerShown: false }} />
       <AuthStack.Screen name="WorkerProfile" component={WorkerProfile} options={{ headerShown: false }} />
       <AuthStack.Screen name="Booking" component={Booking} options={{ headerShown: false }} />
       <AuthStack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
@@ -57,7 +42,6 @@ export default () => (
     
    
     
->>>>>>> Stashed changes
 
 
 
