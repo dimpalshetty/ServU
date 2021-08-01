fd4075fbb6cb8d4bcf1385a8306b4fe20620f280
Shreyasc13 <44752893+Shreyasc13@users.noreply.github.com>
2021-08-01 18:00:06 +0530

    changed class to functional component

--- src/components/gridbuttons.js
+++ src/components/gridbuttons.js
@@ -13,56 +13,56 @@ const height = Dimensions.get('window').height
 
 
 
-const Gridbtn= ({text,icon}) => {
-return(
-  <TouchableOpacity 
-  style={styles.button} >
-  <Icon
-                                name={icon}
-                                size="small"
-                                style={{
-                                    fontSize: 40,
-                            color: '#636BE8',
-                        }}
-                            />
-          <Text style={styles.taskdes}> {text} </Text>
-        </TouchableOpacity>
+const Gridbtn = ({ text, icon }) => {
+  return (
+    <TouchableOpacity
+      style={styles.button} >
+      <Icon
+        name={icon}
+        size="small"
+        style={{
+          fontSize: 40,
+          color: '#636BE8',
+        }}
+      />
+      <Text style={styles.taskdes}> {text} </Text>
+    </TouchableOpacity>
 
-        );
+  );
 }
 
 
 
 const styles = StyleSheet.create({
 
-  
-    button: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontSize: 18,
-      textAlign: 'center',
-      color: 'red',
-      fontWeight: "500",
-      top: 4,
-      borderTopRightRadius: 20,
-      borderBottomLeftRadius: 20,
-      borderBottomRightRadius: 20,
-      borderColor: '#583EF2',
-      borderWidth: 1.5,
-      width: width/3.7,
-      height: height/6
-    },
 
-    taskdes: {
-        marginTop: 2,
-        color: '#583EF2',
-        fontSize: 16,
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 18,
+    textAlign: 'center',
+    color: 'red',
+    fontWeight: "500",
+    top: 4,
+    borderTopRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    borderColor: '#583EF2',
+    borderWidth: 1.5,
+    width: width / 3.7,
+    height: height / 6
+  },
 
-    },
+  taskdes: {
+    marginTop: 2,
+    color: '#583EF2',
+    fontSize: 16,
 
-  
+  },
 
-  
+
+
+
 });
 
 export default Gridbtn;
